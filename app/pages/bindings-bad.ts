cb2443e4b7c36b6342618820751655bd41846c49
Nathan Walker <walkerrunpdx@gmail.com>
2021-03-18 20:24:55 -0700

    chore: bad bindings

--- app/pages/bindings-bad.ts
+++ app/pages/bindings-bad.ts
@@ -109,6 +109,34 @@ export class PracticeModel extends Observable {
 
 	private getTitle(title) {
 		console.log('whoah, I keep executing for:', title);
-		return title;
+		return `${title} ${doPointlessComputationsWithBlocking()[1]}`;
 	}
+}
+
+const iterations = 50;
+const multiplier = 100000000;
+
+function calculatePrimes(iterations, multiplier) {
+	var primes = [];
+	for (var i = 0; i < iterations; i++) {
+		var candidate = i * (multiplier * Math.random());
+		var isPrime = true;
+		for (var c = 2; c <= Math.sqrt(candidate); ++c) {
+			if (candidate % c === 0) {
+				// not prime
+				isPrime = false;
+				break;
+			}
+		}
+		if (isPrime) {
+			primes.push(candidate);
+		}
+	}
+	return primes;
+}
+
+function doPointlessComputationsWithBlocking() {
+	var primes = calculatePrimes(iterations, multiplier);
+	console.log(primes);
+	return primes;
 }
